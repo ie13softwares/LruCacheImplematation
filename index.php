65282a679cc51af1a04ecbf8f5943a3846b2c225
Bala Krishna <prince.balakrishna@gmail.com>
2017-10-12 15:04:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <html>
-<head>
-<script src="lru.js"></script>
-<link rel="stylesheet" type="text/css" href="mystyle.css">
-</head>
-<body>
-Enter a word:
-<input type = "text" id="key">
-<button type ="button" onclick="getMean()">Get Meaning</button>
-<p id = "ans"></p>
-</body>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<head >
+		<script src="lru.js"></script>
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+	</head>
+	
+
+	<title>
+		LRU Cache Testing
+	</title>
+
+	<body>
+		<div>
+		<p>The Program retuns the meaing of the word entered, </br> If the word is not present, It returns Black value. </p>
+		<p style="font-size:20px">Enter a word:<p>
+		<input type = "text" id="key" onfocus="this.value='';">
+		<button type ="button" onclick="getMean()" style="text-align:center;">Get Meaning</button>
+		<p id = "ans"></p>
+		</div>
+	</body>
 </html>
