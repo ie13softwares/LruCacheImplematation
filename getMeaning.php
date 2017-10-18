969993f47a63cc79757f4bfb5511f909ab58489e
Bala Krishna <prince.balakrishna@gmail.com>
2017-10-18 19:36:48 +0530

    Changes made to remove the warning
    Though it was not exactly clear why it is throwing the error, considering the fact that, I have checked the code again for any argument or variable mismatch to the best of my knowledge. The only conclusion, I can reach was some ussue with the XAMP setup and somebug in PHP 5. 
    
    Just rewrote the getMeaning.php code, using Object Oriented approch of PHP 5. This resoved the issue. Please do revert back, if there are anyother warnings.

--- getMeaning.php
+++ getMeaning.php
@@ -1,5 +1,36 @@
 <?php
-	$word = $_GET["word"];
+
+//The Table -> word_dict has two fields, word and meaning respectively, to store word and meaning.
+//Please correct according to your databse details in the order ''hostname, 'username, 'password', 'databse name'
+
+$con = new mysqli('localhost', 'root', 'murari', 'test_db');
+
+if($con->connect_error){
+	die("Connection failed");
+}
+
+
+//
+if(isset($_GET['word'])){
+	$word = $_GET['word'];
+	$sql = "SELECT * FROM word_dict WHERE word = '$word'";
+
+	$result = $con->query($sql);
+	$row = $result->fetch_array();
+		$result = $row["meaning"];
+		echo $result;
+
+
+}
+
+mysqli_close($con);
+
+
+//Previous Code and some other experiments. 
+/*
+/*
+<?php
+  $word = $_GET["word"];
         //Edit the details according to your database setup. 
         $username = "root";
         $password = "murari";
@@ -14,15 +45,38 @@
           $selected = mysqli_select_db($dbhandle, $databasename)
             or die("Could not select test_db");
 
-
           //execute the SQL query and return records
-          $result = mysqli_query($dbhandle, "SELECT * FROM $tablename where word = '$word'");
+          $result = mysqli_query($dbhandle, "SELECT meaning FROM $tablename where word = '$word'") or die("Error: " . mysqli_error($dbhandle));
 
           //fetch tha data from the database
-          while ($row = mysqli_fetch_array($result)) {
+          while ($row = mysqli_fetch_assoc($result)) {
              $result = $row{'meaning'};
              echo $result;
           }
+
           //close the connection
           mysqli_close($dbhandle);
 ?>
+*/
+
+
+
+
+/*
+function dbConnection(){
+	if($mysqli = new mysqli($servername, $username, $password, $dbname)) 
+		return $mysqli; 
+	else 
+		return false;
+}
+
+
+//Calling Database Connection
+if(!dbConnection())
+	exit("<script language='javascript'>alert('Unable to connect to database')</script>");
+else 
+	$con = dbConnection();
+*/
+
+?>
+
